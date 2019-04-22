e342bd716f3e0050fbf19a4e1597cad6f06ab97e
Pedro Gutierrez <pgutierrez@signal.co>
2019-04-21 20:43:03 -0500

    added support with the feature doesnt exists

--- src/main/java/starter/commons/KarateRunner.java
+++ src/main/java/starter/commons/KarateRunner.java
@@ -1,11 +1,13 @@
 package starter.commons;
 
+import com.intuit.karate.Resource;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static com.intuit.karate.FileUtils.scanForFeatureFiles;
 import static org.junit.Assert.assertTrue;
 
 public class KarateRunner {
@@ -42,6 +44,7 @@ public class KarateRunner {
     }
 
     public void run() {
+        isFeatures();
         Results results = Runner.parallel(tags, features, this.threads, KARATE_OUTPUT_PATH);
         assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
     }
@@ -49,7 +52,14 @@ public class KarateRunner {
     public static void runFeature(String featurePath){
         List<String> tags = Arrays.asList(DEFAULT_TAGS);
         List<String> features = Arrays.asList(featurePath);
-        Results results = Runner.parallel(tags, features, DEFAULT_THREADS, KARATE_OUTPUT_PATH);
-        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+        KarateRunner karateRunner = new KarateRunner(tags, features, DEFAULT_THREADS);
+        karateRunner.run();
+    }
+
+    private void isFeatures(){
+        for (String feature:features){
+            List<Resource> features=scanForFeatureFiles(true,feature, Thread.currentThread().getContextClassLoader());
+            assertTrue("Classpath not found:"+feature,!features.isEmpty());
+        }
     }
 }
--- src/test/java/starter/commons/KarateRunnerTest.java
+++ src/test/java/starter/commons/KarateRunnerTest.java
@@ -1,6 +1,8 @@
 package starter.commons;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,11 +11,23 @@ import static org.junit.Assert.*;
 
 public class KarateRunnerTest {
 
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
     @Test
     public void run() {
         List<String> tags = Arrays.asList("~@karate");
         List<String> features = Arrays.asList("classpath:features/karate/load_url.feature");
         KarateRunner karateRunner=new KarateRunner(tags,features,1);
+        karateRunner.run();
+    }
+
+    @Test
+    public void nonExistentClasspath() {
+        List<String> tags = Arrays.asList("~@karate");
+        List<String> features = Arrays.asList("classpath:features/karate/non-existent.feature");
+        KarateRunner karateRunner=new KarateRunner(tags,features,1);
+        exceptionRule.expect(AssertionError.class);
         karateRunner.run();
     }
 }
